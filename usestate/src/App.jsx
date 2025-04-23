bb5dc1be05b39a338a9837ac3c1b06d8d4067404
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-23 12:03:51 -0700

    Project initialized. Dependencies installed and useState imported. Separated imports based on what they are.

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,5 +1,13 @@
+// Importing components
 import RestaurantsContainer from "./components/RestaurantsContainer";
+
+// Importing Data
 import { restaurants } from "./data/data.js";
+
+// Importing Hooks
+import {useState} from "react";
+
+// Importing CSS
 import "./App.css";
 
 function App() {
