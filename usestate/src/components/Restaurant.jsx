f4bdb3f984f74165bfc4a734f92a970d89f7ee50
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-23 12:18:15 -0700

    There were no instructions for Step 2. I copied the solution and wrote comments as to what is happening.

--- usestate/src/components/Restaurant.jsx
+++ usestate/src/components/Restaurant.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
 function Restaurant(props) {
+  const [hours, setHours] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  // Function to handle what happens when the hours button is clicked
+  // This function will toggle the hours state between true and false
+  const handleHours = () => {
+    setHours((prev) => !prev);
+  };
+
+  // Function to handle what happens when the menu button is clicked
+  // This function will toggle the menu state between true and false
+  const handleMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
   return (
     <div className="restaurant">
       <img src={props.restaurant.image} alt={props.name} />
@@ -9,10 +25,13 @@ function Restaurant(props) {
         <li>{props.restaurant.cuisine}</li>
         <li>{props.restaurant.rating}</li>
       </ul>
-      <button>Hours</button>
-      <button>Menu</button>
+      {/* Adding onClick event handlers to the buttons that will display the hours and menu of the restaurant when clicked */}
+      <button onClick={handleHours}>Hours</button>
+      <button onClick={handleMenu}>Menu</button>
+
     </div>
   );
 }
 
 export default Restaurant;
+
