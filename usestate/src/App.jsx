a0dbd4cee55ceeb669594708dea1621ac83a1c36
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-23 12:12:13 -0700

    Created a useState Hook and passed it the data from restaurants in data.js. Created a variable in the RestaurantsContainer component that is tied to the restaurantState of useState .

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -11,9 +11,15 @@ import {useState} from "react";
 import "./App.css";
 
 function App() {
+
+  // Destructuring useState to create a state variable and a function to update it
+  // Using the spread operator (...) to take the content of the data array and put it into a new array
+  const [restaurantState, setRestaurantState] = useState([...restaurants]);
+
   return (
     <div className="App">
-      <RestaurantsContainer />
+      {/* Passing down the restaurantState from useState to the RestaurantsContainer component - This is the component that will be affected by the state change */}
+      <RestaurantsContainer restaurants={restaurantState}/>
     </div>
   );
 }
